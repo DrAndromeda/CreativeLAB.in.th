d76e25862b6d1513d1ada8af19792ebafa0b5e60
DrAndromeda <dr.andromeda@users.noreply.github.com>
2026-09-16 01:19:30 +0700

    Direct deploy: mobile menu fix with full build

---
+++ _next/static/uMSn85q-H4CwyQwlmcEbQ/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002F[hub]","\u002F[locale]\u002F[hub]\u002F[service]","\u002F[locale]\u002Fabout","\u002F[locale]\u002Fcase-studies","\u002F[locale]\u002Fcontact","\u002F[locale]\u002Fjournal","\u002F[locale]\u002Fportfolio","\u002F[locale]\u002Fprivacy","\u002F[locale]\u002Fterms"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
